e6a7cf5f4f381e7caee5285ab10ffa490dd90a2f
durga256 <durgadevi256@gmail.com>
2023-10-25 00:32:19 -0400

    First commit

---
+++ src/assets/index.js
@@ -0,0 +1,70 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import Budweiser from './company/Budweiser.png';
+import Keyvrix from './company/Keyvrix.png';
+import PreludeSys from './company/preludesys.png';
+
+import SymboleoWeb from './symboleoWeb.png';
+import TradeSocial from './TradeSocial.png';
+import ExpenseTracker from './ExpenseTracker.png';
+
+import GaneshABI from './testimonials/Ganesh.jpg';
+import KarmKeyvrix from './testimonials/Karm.jpg';
+import Shan from './testimonials/shan.jpg';
+
+import LinkedInIcon from './linkedin.png';
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  Budweiser,
+  Keyvrix,
+  PreludeSys,
+  SymboleoWeb,
+  TradeSocial,
+  ExpenseTracker,
+  GaneshABI,
+  KarmKeyvrix,
+  Shan,
+  LinkedInIcon
+};
